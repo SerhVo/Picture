02ecd6b7ff13d89f797c954a99ebc6047a0610e0
Serhii Vorotnoi <svorotnoy@gmail.com>
2024-10-24 17:34:19 +0300

    step3: forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
+import forms from "./modules/forms";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -7,4 +8,5 @@ window.addEventListener("DOMContentLoaded", () => {
   modals(); // Запуск модуля модальных окон
   sliders(".feedback-slider-item", "", ".main-prev-btn", ".main-next-btn"); // Запуск модуля слайдера
   sliders(".main-slider-item", "vertical");
+  forms(); // Запуск модуля форм
 });
