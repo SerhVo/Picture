a94235803cb429e7c8fec3d00ff1deaf2dcdec11
Serhii Vorotnoi <svorotnoy@gmail.com>
2024-10-25 12:59:01 +0300

    step4: loading elems

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from "./modules/sliders";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
+import showMoreStyles from "./modules/showMoreStyles";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -14,4 +15,5 @@ window.addEventListener("DOMContentLoaded", () => {
   mask('[name="phone"]'); // Запуск модуля маски
   checkTextInputs('[name="name"]'); // check
   checkTextInputs('[name="message"]'); // check
+  showMoreStyles(".button-styles", ".styles-2"); // Запуск модуля показа больше стилей для карточек
 });
