4776b09ccc44159cf888d6aae05ae5dc7e7f0d63
Serhii Vorotnoi <svorotnoy@gmail.com>
2024-10-29 13:09:53 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,23 @@
-import modals from "./modules/modals";
-import sliders from "./modules/sliders";
-import forms from "./modules/forms";
-import mask from "./modules/mask";
-import checkTextInputs from "./modules/checkTextInputs";
-import showMoreStyles from "./modules/showMoreStyles";
+import modals from './modules/modals';
+import sliders from './modules/sliders';
+import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
+import calc from './modules/calc';
+import filter from './modules/filter';
 
-window.addEventListener("DOMContentLoaded", () => {
-  "use strict";
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
 
-  modals(); // Запуск модуля модальных окон
-  sliders(".feedback-slider-item", "", ".main-prev-btn", ".main-next-btn"); // Запуск модуля слайдера
-  sliders(".main-slider-item", "vertical");
-  forms(); // Запуск модуля форм
-  mask('[name="phone"]'); // Запуск модуля маски
-  checkTextInputs('[name="name"]'); // check
-  checkTextInputs('[name="message"]'); // check
-  showMoreStyles(".button-styles", "#styles .row"); // Запуск модуля показа больше стилей для карточек
+    modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '#styles .row');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
+    filter();
 });
--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,64 +1,72 @@
 const sliders = (slides, dir, prev, next) => {
-  let slideIndex = 1,
-  paused = false;
-  const items = document.querySelectorAll(slides);
+    let slideIndex = 1,
+        paused = false;
 
-  function showSlides(n) {
-    if (n > items.length) {
-      slideIndex = 1;
+    const items = document.querySelectorAll(slides);
+          
+    function showSlides(n) {
+        if (n > items.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = items.length;
+        }
+
+        items.forEach(item => {
+            item.classList.add("animated");
+            item.style.display = "none";
+        });
+
+        items[slideIndex - 1].style.display = 'block';
     }
-    if (n < 1) {
-      slideIndex = items.length;
+
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
     }
-    items.forEach((item) => {
-      item.style.display = "none";
-      item.classList.add("animated", "fadeIn");
-    });
-    items[slideIndex - 1].style.display = "block";
-  }
-  showSlides(slideIndex);
 
-  function plusSlides(n) {
-    showSlides((slideIndex += n));
-  }
-  try {
-    const prevButton = document.querySelector(prev),
-      nextButton = document.querySelector(next);
+    try {
+        const prevBtn = document.querySelector(prev),
+              nextBtn = document.querySelector(next);
 
-    prevButton.addEventListener("click", () => {
-      plusSlides(1);
+        prevBtn.addEventListener('click', () => {
+            plusSlides(-1);
+            items[slideIndex - 1].classList.remove('slideInLeft');
+            items[slideIndex - 1].classList.add('slideInRight');
+        });
 
-      items[slideIndex - 1].classList.remove("slideInRight");
-      items[slideIndex - 1].classList.add("slideInLeft");
-    });
-    nextButton.addEventListener("click", () => {
-      plusSlides(1);
-      items[slideIndex - 1].classList.remove("slideInLeft");
-      items[slideIndex - 1].classList.add("slideInRight");
-    });
-  } catch (e) {}
+        nextBtn.addEventListener('click', () => {
+            plusSlides(1);
+            items[slideIndex - 1].classList.remove('slideInRight');
+            items[slideIndex - 1].classList.add('slideInLeft');
+        });
+    } catch(e){}
 
-  function activateAnimation() {
-    if (dir === "vertical") {
-      paused = setInterval(function () {
-        plusSlides(1);
-        items[slideIndex - 1].classList.add("fadeInLeftBig");
-      }, 5000);
-    } else {
-      paused = setInterval(function () {
-        plusSlides(1);
-        items[slideIndex - 1].classList.remove("slideInRight");
-        items[slideIndex - 1].classList.add("slideInLeft");
-      }, 5000);
+    function activateAnimation() {
+        if (dir === 'vertical') {
+            paused = setInterval(function() {
+                plusSlides(1);
+                items[slideIndex - 1].classList.add('slideInDown');
+            }, 3000);
+        } else {
+            paused = setInterval(function() {
+                plusSlides(1);
+                items[slideIndex - 1].classList.remove('slideInRight');
+                items[slideIndex - 1].classList.add('slideInLeft');
+            }, 3000);
+        }
     }
-    };
-
-  activateAnimation();
-  items[0].parentNode.addEventListener("mouseenter", () => {
-    clearInterval(paused);
-  });
-  items[0].parentNode.addEventListener("mouseleave", () => {
     activateAnimation();
-  });
+
+    items[0].parentNode.addEventListener('mouseenter', () => {
+        clearInterval(paused);
+    });
+    items[0].parentNode.addEventListener('mouseleave', () => {
+        activateAnimation();
+    });
+
 };
+
 export default sliders;
